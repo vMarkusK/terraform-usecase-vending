c3513017b65721931281a63a942772decae2dc00
Markus Kraus <markus.kraus@gmail.com>
2025-05-11 20:59:54 +0200

    base

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.25.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.3"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 3.0.2"
+    }
+  }
+  required_version = ">= 1.10.5"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+
+  storage_use_azuread = true
+}
+
+data "azuread_client_config" "this" {}
---
+++ valiables.tf
@@ -0,0 +1,10 @@
+variable "subscription_id" {
+  description = "Subscription ID for all resources"
+  type        = string
+}
+
+variable "location" {
+  description = "Location for all resources"
+  type        = string
+  default     = "germywestcentral"
+}
