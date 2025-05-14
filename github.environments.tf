655a0f4a55676cb7fea686de445572da3d2fe7a4
Markus Kraus <markus.kraus@gmail.com>
2025-05-14 21:00:36 +0200

    env secrets

--- github.environments.tf
+++ github.environments.tf
@@ -28,3 +28,48 @@ resource "github_actions_environment_variable" "tfbackend" {
   variable_name = "BACKEND_FILE"
   value         = format("%s.tfbackend", github_repository_environment.this[each.key].environment)
 }
+
+resource "github_actions_environment_secret" "subscription_id" {
+  for_each = github_repository_environment.this
+
+  repository      = local.repository_name
+  environment     = github_repository_environment.this[each.key].environment
+  secret_name     = "AZURE_SUBSCRIPTION_ID"
+  plaintext_value = var.subscription_id
+}
+
+resource "github_actions_environment_secret" "tenant_id" {
+  for_each = github_repository_environment.this
+
+  repository      = local.repository_name
+  environment     = github_repository_environment.this[each.key].environment
+  secret_name     = "AZURE_TENANT_ID"
+  plaintext_value = data.azuread_client_config.this.tenant_id
+}
+
+resource "github_actions_environment_secret" "client_id" {
+  for_each = github_repository_environment.this
+
+  repository      = local.repository_name
+  environment     = github_repository_environment.this[each.key].environment
+  secret_name     = "AZURE_CLIENT_ID"
+  plaintext_value = azuread_application.this.client_id
+}
+
+resource "github_actions_environment_secret" "state_rg" {
+  for_each = github_repository_environment.this
+
+  repository      = local.repository_name
+  environment     = github_repository_environment.this[each.key].environment
+  secret_name     = "STATE_RESOURCE_GROUP"
+  plaintext_value = azurerm_resource_group.this.name
+}
+
+resource "github_actions_environment_secret" "state_st" {
+  for_each = github_repository_environment.this
+
+  repository      = local.repository_name
+  environment     = github_repository_environment.this[each.key].environment
+  secret_name     = "STATE_STORAGE_ACCOUNT"
+  plaintext_value = azurerm_storage_account.this.name
+}
