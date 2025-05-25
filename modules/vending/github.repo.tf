18529f6207bc2310bad8b804754f30b00abe33bb
Markus Kraus <markus.kraus@gmail.com>
2025-05-25 20:59:50 +0200

    use default main branch

--- modules/vending/github.repo.tf
+++ modules/vending/github.repo.tf
@@ -7,20 +7,8 @@ resource "github_repository" "this" {
   auto_init              = true
 }
 
-resource "github_branch" "main" {
-  repository = github_repository.this.name
-  branch     = "main"
-}
-
-resource "github_branch_default" "default" {
-  repository = github_repository.this.name
-  branch     = github_branch.main.branch
-}
-
 resource "github_actions_variable" "application_name" {
   repository    = github_repository.this.name
   variable_name = "APPLICATION_NAME"
   value         = var.usecase
-
-  depends_on = [github_branch_default.default]
 }
