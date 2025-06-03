ffd74a4f788e86894a1bd2a0bd3dace685c875a9
Markus Kraus <markus.kraus@gmail.com>
2025-06-03 20:07:51 +0200

    branch protection

--- modules/vending/github.repo.tf
+++ modules/vending/github.repo.tf
@@ -12,3 +12,47 @@ resource "github_actions_variable" "application_name" {
   variable_name = "APPLICATION_NAME"
   value         = var.usecase
 }
+
+resource "github_repository_ruleset" "this" {
+  name        = "protect-branch"
+  repository  = github_repository.this.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    pull_request {
+    }
+  }
+
+  depends_on = [
+    github_repository_file.gitignore,
+    github_repository_file.tflint,
+    github_repository_file.vscode_settings,
+    github_repository_file.github_devcontainer,
+    github_repository_file.github_env_tfvars,
+    github_repository_file.github_env_tfbackend,
+    github_repository_file.github_tf_files,
+    github_repository_file.github_wf_files
+  ]
+
+  lifecycle {
+    replace_triggered_by = [
+      github_repository_file.gitignore,
+      github_repository_file.tflint,
+      github_repository_file.vscode_settings,
+      github_repository_file.github_devcontainer,
+      github_repository_file.github_env_tfvars,
+      github_repository_file.github_env_tfbackend,
+      github_repository_file.github_tf_files,
+      github_repository_file.github_wf_files
+    ]
+  }
+
+}
